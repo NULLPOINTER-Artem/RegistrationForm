f51f5093c6331655603814b6b14161bea1b1ac0e
Artem Orlov <universalism123@gmail.com>
2020-10-30 12:23:24 +0200

    commit

--- index.js
+++ index.js
@@ -1,81 +1,151 @@
 window.addEventListener('DOMContentLoaded', init);
 
-let signUpBtn = document.getElementById('signUpBtn');
+let message;
+let form;
 
 function init() {
-    let form = document.getElementById('signUpForm');
-    signUpBtn = document.getElementById('signUpBtn');
+    form = document.getElementById('signUpForm');
+    message = document.createElement('span');
+    message.classList.add('msgError');
 
-    signUpBtn.addEventListener('submit', );
+    form.oninput = function(event) {
+        checkOnValidForm(event);
 
-    form.email.oninput = checkFormOnValid(form.email);
-    form.name.oninput = checkFormOnValid(form.name);
-    form.password.oninput = checkFormOnValid(form.password);
-    form.rePassword.oninput = checkFormOnValid(form.rePassword);
+        checkButton(form.elements);
+    }
+
+    form.onsubmit = function(event) {
+        event.preventDefault();
+        let obj = {};
+
+        [].forEach.call(form.elements, (item) => {
+            if(item.type != "submit") {
+                obj[item.name] = item.value;
+            }
+        })
+
+        console.log(obj);
+    }
 }
 
-function showMessageError(targetElement, error) {
-    switch(targetElement.name) {
-        case 'email': {
-            error.textContent = 'This input should not be empty and fit to format of email!';
-            error.style = 'display: block';
-            break;
-        }
-        case 'name': {
-            error.textContent = 'This input should not be empty!'
-            error.style = 'display: block';
-            break;
-        }
-        case 'password': {
-            error.textContent = 'This input should not be empty!'
-            error.style = 'display: block';
-            break;
-        }
-        case 'rePassword': {
-            error.textContent = 'This input should not be empty and repeat above entered the password!'
-            error.style = 'display: block';
-            break;
+function checkButton(elements) {
+    let button = document.getElementById('signUpBtn');
+
+    let hasSomeEmpty = [].some.call(elements, (item) => {
+        return item.value.length == 0;
+    })
+
+    let hasSomeError = [].some.call(elements, (item) => {
+        return item.classList.contains('error');
+    })
+
+    if (hasSomeEmpty || hasSomeError) {
+        button.setAttribute("disabled", "");
+    } else {
+        button.removeAttribute("disabled");
+    }
+}
+
+function checkOnValidForm(event) {
+    if(!checkOnEmpty(event)) {
+        switch (event.target.name) {
+            case 'email': {
+                if(!checkOnValidEmail(event.target.value)) {
+                    event.target.classList.add('error');
+                    message.textContent = "Entered email does not fit to the format";
+                    message.style = 'display: block';
+                    event.target.after(message);
+                } else {
+                    event.target.classList.remove('error');
+                    message.style = 'display: none';
+                }
+
+                break;
+            }
+            case 'name': {
+                if(!checkOnValidName(event.target.value)) {
+                    event.target.classList.add('error');
+                    message.textContent = "Entered name is not correct";
+                    message.style = 'display: block';
+                    event.target.after(message);
+                } else {
+                    event.target.classList.remove('error');
+                    message.style = 'display: none';
+                }
+
+                break;
+            }
+            case 'rePassword': {
+                if(!checkOnConfirmPassword(event.target.value)) {
+                    event.target.classList.add('error');
+                    message.textContent = "Entered password does not match";
+                    message.style = 'display: block';
+                    event.target.after(message);
+                } else {
+                    event.target.classList.remove('error');
+                    message.style = 'display: none';
+                }
+
+                break;
+            }
         }
     }
 }
 
-function hideMessageError(targetElement, error) {
-    switch (targetElement.name) {
-        case 'email': {
-            error.style = 'display: none';
-            break;
-        }
-        case 'name': {
-            error.style = 'display: none';
-            break;
-        }
-        case 'password': {
-            error.style = 'display: none';
-            break;
-        }
-        case 'rePassword': {
-            error.style = 'display: none';
-            break;
-        }
+function checkOnConfirmPassword(rePassword) {
+    let valid = true;
+    let password = '';
+
+    [].find.call(form.elements, (item) => {
+        if(item.name == "password") {
+            password = item.value;
+        } 
+    });
+
+    if(rePassword != password) {
+        valid = false;
     }
+
+    return valid;
 }
 
-function checkFormOnValid(targetElement) {
-    let error = document.createElement('span');
-    error.classList.add('msgError');
-    targetElement.after(error);
+function checkOnValidEmail(email) {
+    let valid = true;
+    let exampleOfValidEmail = /^([a-zA-Z0-9_\.\-]{3,15})+\@(([a-zA-Z0-9\-]{3,10})+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-    return function() {
-        if (targetElement.value.length == 0) {
-            targetElement.classList.add('error');
-            signUpBtn.setAttribute("disabled", "");
-
-            showMessageError(targetElement, error);
-        } else {
-            targetElement.classList.remove('error');
-            signUpBtn.removeAttribute("disabled");
-
-            hideMessageError(targetElement, error);
-        }
+    if (!exampleOfValidEmail.test(email)) {
+        valid = false;
     }
+
+    return valid;
 }
+
+function checkOnValidName(name) {
+    let valid = true;
+    let exampleOfValidEmail = /^[a-zA-Z0-9_\.\-]{2,15}$/;
+
+    if(!exampleOfValidEmail.test(name)) {
+        valid = false;
+    }
+
+    return valid;
+}
+
+function checkOnEmpty(event) {
+    let empty = true;
+
+    if (!event.target.value.length) {
+        event.target.classList.add('error');
+        message.textContent = 'This field have to be filled!';
+        message.style = 'display: block';
+        event.target.after(message);
+    } else {
+        empty = false;
+
+        event.target.classList.remove('error');
+        message.style = 'display: none';
+    }
+
+    return empty;
+}
+
